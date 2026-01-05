bc401e402c1877ff26829af6be07dd410a1cfa9d
Tuongtapcode <cocghe2468@gmail.com>
2026-01-05 07:27:27 +0700

    Config order-service with Spring Boot and MySQL

--- order-services/src/main/java/com/nnt/microservices/order/repository/OrderRepository.java
+++ order-services/src/main/java/com/nnt/microservices/order/repository/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.nnt.microservices.order.repository;
 
-public interface OrderRepository {
+import com.nnt.microservices.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    
 }
--- order-services/src/main/java/com/nnt/microservices/order/service/OrderService.java
+++ order-services/src/main/java/com/nnt/microservices/order/service/OrderService.java
@@ -1,4 +1,27 @@
 package com.nnt.microservices.order.service;
 
+import com.nnt.microservices.order.dto.OrderRequest;
+import com.nnt.microservices.order.model.Order;
+import com.nnt.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+        order.setSkucCode(orderRequest.skuCode());
+
+        orderRepository.save(order);
+
+    }
 }
